ce25535382d32bc2d674b77705dd6af2cfef7379
Marek Kustosz <markust71@hotmail.com>
2023-11-09 00:12:20 +0100

    set up binance api

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    commonjsOptions: {
+      include: [],
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
+  plugins: [react(), nodePolyfills()],
+  server: {
+    port: 3000,
+  },
+});
