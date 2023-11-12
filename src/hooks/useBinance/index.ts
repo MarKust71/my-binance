e0c19b70710caf0369210f78309036434537bad4
Marek Kustosz <markust71@hotmail.com>
2023-11-12 12:42:52 +0100

    binance no extra libs

--- src/hooks/useBinance/index.ts
+++ src/hooks/useBinance/index.ts
@@ -1,36 +1,50 @@
-/* eslint-disable camelcase */
-import Binance from 'binance-api-node';
 import { useEffect, useState } from 'react';
 
-const { VITE_BINANCE_API_KEY: api_key, VITE_BINANCE_API_SECRET: api_secret } = import.meta.env;
+export const {
+  VITE_BINANCE_API_KEY: BINANCE_API_KEY,
+  VITE_BINANCE_API_SECRET: BINANCE_API_SECRET,
+  VITE_BINANCE_API_URL: BINANCE_API_URL,
+} = import.meta.env;
+
+export const READY_STATE = ['CONNECTING', 'OPEN', 'CLOSING', 'CLOSED'];
+
+const coin = 'BTCTUSD';
+const url = `wss://stream.binance.com:9443/ws/${coin.toLowerCase()}@trade`;
 
 export const useBinance = () => {
-  const [curDayClose, setCurDayClose] = useState('');
+  const [price, setPrice] = useState('');
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
-  const binanceClient = Binance({
-    apiKey: api_key,
-    apiSecret: api_secret,
-  });
+  const close = () => {
+    if (socket) {
+      socket.close();
+    }
+  };
 
-  const binanceClientWSClean = binanceClient.ws.miniTicker('BTCTUSD', (miniTicker) => {
-    setCurDayClose(miniTicker.curDayClose);
-  });
+  const onMessage = (event: MessageEvent) => {
+    const data = JSON.parse(event.data);
+
+    setPrice(() => data.p);
+  };
 
   useEffect(() => {
+    const ws = new WebSocket(url);
+
+    setSocket(() => ws);
+
+    ws.onmessage = (event) => {
+      onMessage(event);
+    };
+
     return () => {
-      binanceClientWSClean();
+      ws.close();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const getAccountInfo = async () => {
-    return await binanceClient.accountInfo();
-  };
-
   return {
-    binanceClient,
-    binanceClientWSClean,
-    curDayClose,
-    getAccountInfo,
+    price,
+    readyState: socket?.readyState,
+    close,
   };
 };
